c0ad1cf8cc2c6e1d3a4c6d71306f86c66fee1924
DANIEL TRINDADE <danielbt751@gmail.com>
2026-04-12 20:25:31 -0300

    condições

---
+++ Aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 70
+console.log(`A velocidade do seu carro é de ${vel} Km/h.`)
+if (vel > 60){ //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!!`)
+}
+
+console.log(`Dirija com cuidado!`)
